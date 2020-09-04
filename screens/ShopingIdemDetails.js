a2057ea5bd1fe79b4f96ff064e997d99f36dc1dd
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-09-04 23:41:26 +0200

    add global styles

--- screens/ShopingIdemDetails.js
+++ screens/ShopingIdemDetails.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function ShopingItemDetails() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>ShopingItemDetails Screen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-});
--- screens/todoList.js
+++ screens/todoList.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function TodoList() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>TodoList Screen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-});
