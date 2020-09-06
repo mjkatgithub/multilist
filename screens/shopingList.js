b0e617537e43ccce4adf92b9cf5c863d37666ac2
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-09-06 20:06:48 +0200

    add modal

--- screens/shopingList.js
+++ screens/shopingList.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Text, FlatList, TouchableOpacity } from 'react-native';
+import { Text, FlatList, TouchableOpacity, Modal, StyleSheet, View} from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 import Container from '../shared/container';
+import { MaterialIcons } from '@expo/vector-icons';
+import { ScreenStackHeaderRightView } from 'react-native-screens';
 
 export default function ShopiingList({ navigation }) {
-
+  const [modalOpen, setModalOpen] = useState(false);
   const [shopingItem, setShopingItem] = useState([
     { product: 'wurst', manufacturer: 'hans', notes: 'happa-happa', _id: '1' },
     { product: 'milch', manufacturer: 'mueller', notes: 'schluerf', _id: '2' },
@@ -14,6 +16,19 @@ export default function ShopiingList({ navigation }) {
 
   return (
     <Container>
+
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name='close'
+            size={24}
+            style={styles.modalClose}
+            onPress={() => setModalOpen(false)}
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+
       <FlatList
         keyExtractor={ (item) => item._id }
         data={shopingItem}
@@ -25,6 +40,42 @@ export default function ShopiingList({ navigation }) {
           </TouchableOpacity>
         )}
       />
+
+      <MaterialIcons
+        name='add'
+        size={24}
+        style={styles.modalAdd}
+        onPress={() => setModalOpen(true)}
+      />
+
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  modalAdd: {
+    color: 'white',
+    borderWidth: 1,
+    borderColor: 'darkgreen',
+    padding: 19,    
+    width: 60,  
+    height: 60,   
+    borderRadius: 30,                                                
+    position: 'absolute',                                          
+    bottom: 10,                                                    
+    right: 10, 
+    backgroundColor: 'rgba(0, 128, 0, 0.8)',
+  },
+  modalClose: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center'
+  },
+  modalContent: {
+    flex: 1
+  }
+
+});
--- styles/global.js
+++ styles/global.js
@@ -15,4 +15,3 @@ export const globalStyles = StyleSheet.create({
     lineHeight: 20
   }
 });
-
