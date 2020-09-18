472519fe3e9f8e8425439a86ea5a54190e8ad5f4
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-09-18 11:31:21 +0200

    add scrollview to shoppingForm

--- screens/shoppingItemForm.js
+++ screens/shoppingItemForm.js
@@ -3,6 +3,7 @@ import { StyleSheet, Button, TextInput, View, Text} from 'react-native';
 import { globalStyles } from '../styles/global';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const shoppingItemSchema = yup.object({
     product: yup.string()
@@ -20,58 +21,60 @@ const shoppingItemSchema = yup.object({
 export default function ShoppingItemForm({ addShopingItem }) {
   return(
     <View style={globalStyles.container}>
-      <Formik
-        initialValues={{product: '', manufacturer: '', amount: '', notes: '', _id: ''}}
-        validationSchema={shoppingItemSchema}
-        onSubmit={ (values, actions) => {
-          actions.resetForm();
-          addShopingItem(values);
-        }}
-      >
-        {(formikProps) => (
-          <View>
-            <TextInput
-              style={globalStyles.input}
-              placeholder='Product'
-              onChangeText={formikProps.handleChange('product')}
-              value={formikProps.values.product}
-              onBlur={formikProps.handleBlur('product')}
-            />
-            <Text style={globalStyles.errorText}>{ formikProps.touched.product && formikProps.errors.product }</Text>
+      <ScrollView>
+        <Formik
+          initialValues={{ product: '', manufacturer: '', amount: '', notes: '', _id: '' }}
+          validationSchema={shoppingItemSchema}
+          onSubmit={(values, actions) => {
+            actions.resetForm();
+            addShopingItem(values);
+          }}
+        >
+          {(formikProps) => (
+            <View>
+              <TextInput
+                style={globalStyles.input}
+                placeholder='Product'
+                onChangeText={formikProps.handleChange('product')}
+                value={formikProps.values.product}
+                onBlur={formikProps.handleBlur('product')}
+              />
+              <Text style={globalStyles.errorText}>{formikProps.touched.product && formikProps.errors.product}</Text>
 
-            <TextInput
-              style={globalStyles.input}
-              placeholder='Manufacturer'
-              onChangeText={formikProps.handleChange('manufacturer')}
-              value={formikProps.values.manufacturer}
-              onBlur={formikProps.handleBlur('manufacturer')}
-            />
-            <Text style={globalStyles.errorText}>{ formikProps.touched.manufacturer && formikProps.errors.manufacturer }</Text>
+              <TextInput
+                style={globalStyles.input}
+                placeholder='Manufacturer'
+                onChangeText={formikProps.handleChange('manufacturer')}
+                value={formikProps.values.manufacturer}
+                onBlur={formikProps.handleBlur('manufacturer')}
+              />
+              <Text style={globalStyles.errorText}>{formikProps.touched.manufacturer && formikProps.errors.manufacturer}</Text>
 
-            <TextInput
-            style={globalStyles.input}
-            placeholder='amount (1-99)'
-            onChangeText={formikProps.handleChange('amount')}
-            value={formikProps.values.amount}
-            keyboardType='numeric'
-            onBlur={formikProps.handleBlur('amount')}
-            />
-            <Text style={globalStyles.errorText}>{ formikProps.touched.amount && formikProps.errors.amount }</Text>
+              <TextInput
+                style={globalStyles.input}
+                placeholder='amount (1-99)'
+                onChangeText={formikProps.handleChange('amount')}
+                value={formikProps.values.amount}
+                keyboardType='numeric'
+                onBlur={formikProps.handleBlur('amount')}
+              />
+              <Text style={globalStyles.errorText}>{formikProps.touched.amount && formikProps.errors.amount}</Text>
 
-            <TextInput
-              multiline
-              style={globalStyles.input}
-              placeholder='Notes'
-              onChangeText={formikProps.handleChange('notes')}
-              value={formikProps.values.notes}
-              onBlur={formikProps.handleBlur('notes')}
-            />
-            <Text style={globalStyles.errorText}>{ formikProps.touched.notes && formikProps.errors.notes }</Text>
+              <TextInput
+                multiline
+                style={globalStyles.input}
+                placeholder='Notes'
+                onChangeText={formikProps.handleChange('notes')}
+                value={formikProps.values.notes}
+                onBlur={formikProps.handleBlur('notes')}
+              />
+              <Text style={globalStyles.errorText}>{formikProps.touched.notes && formikProps.errors.notes}</Text>
 
-            <Button title='submit' color='green' onPress={formikProps.handleSubmit} />
-          </View>
-        )}
-      </Formik>
+              <Button title='submit' color='green' onPress={formikProps.handleSubmit} />
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
     </View>
   );
 }
